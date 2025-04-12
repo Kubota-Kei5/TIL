f011974929a5ed630a78fa910a24907ebfb46057
kubotake-desktop <keigo1003560@gmail.com>
2025-04-12 19:15:45 +0900

    UdemyのReact講座

--- front/um-react/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
+++ front/um-react/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,19 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita
+        nam hic saepe delectus quaerat excepturi sed earum sequi. Excepturi non
+        delectus beatae. Ullam ducimus incidunt provident optio, beatae earum.
+      </p>
+    </React.Fragment>
   );
 };
 
--- front/um-react/react-guide-material/04_react_basic/src/080_expr_in_jsx/start/Example.jsx
+++ front/um-react/react-guide-material/04_react_basic/src/080_expr_in_jsx/start/Example.jsx
@@ -1,5 +1,10 @@
 import Child from "./components/Child";
+import Expression from "./components/Expressions";
 
-const Example = () => <Child />;
-
+const Example = () => (
+  <>
+    <Child />
+    <Expression />
+  </>
+);
 export default Example;
